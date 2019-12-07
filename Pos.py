45a7f42d6ea9b29bdb16e0ee19f9c25ab0d1dbf6
ndd <tarahjjeon96@gmail.com>
2019-12-07 17:13:03 +0900

    Sitting

--- Pos.py
+++ Pos.py
@@ -1,31 +1,53 @@
 import RPi.GPIO as GPIO
 import time
+import dht11
 from camera import *
+import threading
 
 cnt = 0
+instance = dht11.DHT11(pin=21)
 
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     
-def tilt():
-    global cnt
-    if (GPIO.input(20) == True):
-        print("기울임!")
-        cnt += 1
-        camera(cnt)
+def sit():
+    global instance
+    result = instance.read()
+    temp_pre=0
+    if result.is_valid():
+        temp_pre = result.humidity
+    print("처음 습도 : "+str(temp_pre))
+    flag=True
+    while flag:
+        result = instance.read()
+        if result.is_valid():
+            temp_post = result.humidity
+            if temp_post-temp_pre>1:
+                print("sitting")
+                flag=False
+                
+    print("틸트수행")
         
-    else:
-        print("안기울임!")
+#     tilt()
+    
+# def tilt():
+#     global cnt
+#     if (GPIO.input(20) == True):
+#         print("기울임!")
+#         cnt += 1
+#         camera(cnt)
+        
+#     else:
+#         print("안기울임!")
 
 if __name__=="__main__":
     GPIO.setwarnings(False)
     setup()
     try:
         while True:
-            tilt()
+            sit()
             time.sleep(0.5)
     except KeyboardInterrupt:
         GPIO.cleanup()
-    
     
